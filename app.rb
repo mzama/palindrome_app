728d881ee72e78f29ff2231638bb2b4f5ea0d126
Matteo Zamaria <matteozamaria@gmail.com>
2021-01-21 01:32:20 -0500

    Add a nav and variable titles.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 
 get '/' do
+    @title = 'Home'
     #Any page will be placed into layout.erb by default, so if we had one layout defined there, this could just be
     #erb :index
     #However, I have instead changed the name to default_page.erb to demonstrate how to inherit a specific layout
@@ -8,9 +9,11 @@ get '/' do
 end
 
 get '/about' do
+    @title = 'About'
     erb :about, :layout => :default_page
 end
 
 get '/palindrome' do
+    @title = 'Palindrome Detector'
     erb :palindrome, :layout => :default_page
 end
